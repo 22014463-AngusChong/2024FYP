d91ae70f505035803992375e5b1931f146af5fc0
22020981-Gemma <22020981@myrp.edu.sg>
2024-06-09 00:16:08 +0800

    update add funds

--- CrowdFunding/crowdfunding-app/src/pages/AddFunds.js
+++ CrowdFunding/crowdfunding-app/src/pages/AddFunds.js
@@ -1,56 +1,78 @@
 import React, { Component } from 'react';
+
 class AddFunds extends Component {
-    render() {
-        return (
-        <div id="content">
-            <p> Total no of funds {this.props.fundsCount} </p>
-            <h1>Add Fund </h1>
-            <form onSubmit={(event) => {
-                event.preventDefault()
-                const name = this.fundsName.value
-                const picName = this.fundsPic.value                  
-                const price = window.web3.utils.toWei(this.fundsPrice.value.toString(), 'ether')
-                this.props.addFunds(name, picName, price)
-            }}> {/*end of form */}
-                <div className="form-group mr-sm-2">
-                    <input
-                        id="fundsName"
-                        type="text"
-                        ref={(input) => { this.fundsName = input }}
-                        className="form-control"
-                        placeholder="Fund Name"
-                        required />
+  constructor(props) {
+    super(props);
+    this.state = { message: '' }; // Add state for message
+  }
+
+  render() {
+    const { message } = this.state; // Destructure message from state
+
+    return (
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <div className="card shadow p-3 mb-5 bg-white rounded">
+          <p> Total no of Campaign {this.props.fundsCount} </p>
+          <h1>Add Campaign </h1>
+          {message && <p className="alert alert-success">{message}</p>} {/* Display message if it exists */}
+          <form onSubmit={async (event) => {
+            event.preventDefault();
+            const name = this.fundsName.value;
+            const picName = this.fundsPic.value;
+            const price = window.web3.utils.toWei(this.fundsPrice.value.toString(), 'ether');
+
+            try {
+              await this.props.addFunds(name, picName, price);
+              this.setState({ message: 'Campaign added successfully!' }); // Set success message
+            } catch (error) {
+              console.error('Error adding campaign:', error);
+              this.setState({ message: 'Error adding campaign, please try again.' }); // Set error message
+            }
+          }}>
+            <div className="form-group">  {/* Group all inputs */}
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <input
+                    id="fundsName"
+                    type="text"
+                    ref={(input) => { this.fundsName = input }}
+                    className="form-control"
+                    placeholder="Fund Name"
+                    required
+                  />
                 </div>
-                <br />
-                <div className="form-group mr-sm-2">
-                    <input
-                        id="fundsPic"
-                        type="text"
-                        ref={(input) => { this.fundsPic = input }}
-                        className="form-control"
-                        placeholder="Fund Picture"
-                        required />
+                <div className="row">
+                    <div className="col-md-6 mb-3">
+                  <input
+                    id="fundsPic"
+                    type="text"
+                    ref={(input) => { this.fundsPic = input }}
+                    className="form-control"
+                    placeholder="Fund Picture"
+                    required
+                  />
+                    </div>
                 </div>
-                <br />
-                <br />
-                <div className="form-group mr-sm-2">
-                    <input
-                        id="fundsPrice"
-                        type="text"
-                        ref={(input) => { this.fundsPrice = input }}
-                        className="form-control"
-                        placeholder="Fund Price"
-                        required />
+              </div>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <input
+                    id="fundsPrice"
+                    type="text"
+                    ref={(input) => { this.fundsPrice = input }}
+                    className="form-control"
+                    placeholder="Fund Price"
+                    required
+                  />
                 </div>
-                
-                <br />
-                <br />
-                <button type="submit" className="btn btn-primary">Add Fund</button>
-            </form>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Add Campaign</button>
+          </form>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default AddFunds;
-
