841f7c4f6d19cc3eedda1c0e9ea8548ed5e481a1
22036221-Aricia <22036221@myrp.edu.sg>
2024-06-12 11:21:33 +0800

    update

--- CrowdFunding/crowdfunding-app/src/NavBar.js
+++ CrowdFunding/crowdfunding-app/src/NavBar.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import Layout from "./pages/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 class NavBar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'lightgrey' }}>
-                <div class="container">
-                    <img src="logo-crowdfunding.png" width="200" height="200" />
-                    <span class="navbar-brand mb-0 h1">Crowdfunding Platform </span>
-                    <a class="nav-link active" href="/">Home</a>
-                    <a class="nav-link active" href="/AddFunds">Add Fund</a>
-                    <a class="nav-link active" href="/AboutUs">About Us</a>
-                    <p class="nav-link" href="#"> Projects: <span id="account"> {this.props.account} </span> </p>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'lightgreen' }}>
+                <div className="container d-flex justify-content-center align-items-center">
+                    <div className="navbar-nav w-100 d-flex justify-content-between align-items-center">
+                        <div className="nav-item d-none d-lg-block">
+                            <a className="nav-link active" style={{ color: 'black' }} href="/">Home</a>
+                            <a className="nav-link active" style={{ color: 'black' }} href="/AddFunds">Add Fund</a>
+                            <a className="nav-link active" style={{ color: 'black' }} href="/AboutUs">About Us</a>
+                        </div>
+                        <div className="nav-item logo">
+                            <img src="logo-crowdfunding.png" width="100" height="auto" alt="Logo" />
+                        </div>
+                        <div className="nav-item">
+                        
+                        </div>
+                    </div>
                 </div>
             </nav>
-        )
+        );
     }
 }
-export default NavBar;  
+
+export default NavBar;
