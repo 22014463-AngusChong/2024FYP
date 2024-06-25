fefc3bb43d3c5cdefb7715315da7884a0eeb636c
22036221-Aricia <22036221@myrp.edu.sg>
2024-06-25 13:01:45 +0800

    update

--- CrowdFunding/crowdfunding-app/src/NavBar.js
+++ CrowdFunding/crowdfunding-app/src/NavBar.js
@@ -2,32 +2,33 @@ import React, { Component } from 'react';
 import Layout from "./pages/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'lightgreen' }}>
-                <div className="container d-flex justify-content-center align-items-center">
-                    <div className="navbar-nav w-100 d-flex justify-content-between align-items-center">
-                        <div className="nav-item d-none d-lg-block">
-                            <a className="nav-link active" style={{ color: 'black' }} href="/">Home</a>
-                            <a className="nav-link active" style={{ color: 'black' }} href="/AddFunds">Add Fund</a>
-                        </div>
-                        <div className="nav-item logo">
+                <div className="container d-flex justify-content-between align-items-center">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav w-100 d-flex justify-content-between align-items-center">
+                            <li className="nav-item">
+                                <a className="nav-link active" style={{ color: 'black' }} href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" style={{ color: 'black' }} href="/AddFunds">Add Funds</a>
+                            </li>
+                            <div className="nav-item logo">
                         <a href="/">
                                 <img src="logo-crowdfunding.png" width="200" height="200" alt="Logo" />
                             </a>
                         </div>
-                        <div className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle active" style={{ color: 'black' }} href="/AboutUs" id="aboutUsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                About Us
-                            </a>
-                            {/* <ul className="dropdown-menu" aria-labelledby="aboutUsDropdown">
-                                <li><a className="dropdown-item" href="/AboutUs/OurTeam">Our Team</a></li>
-                                <li><a className="dropdown-item" href="/AboutUs/OurStory">Our Story</a></li>
-                                <li><a className="dropdown-item" href="/AboutUs/Careers">Careers</a></li>
-                            </ul> */}
+                            <li className="nav-item dropdown"></li>
+                        <a className="nav-link active" style={{ color: 'black' }} href="/AboutUs">AboutUs
+                        </a>
                             <a className="nav-link active" style={{ color: 'black' }} href="/ContactUs">Contact Us</a>
-                        </div>
+                        </ul>
                     </div>
                 </div>
             </nav>
@@ -36,4 +37,3 @@ class NavBar extends Component {
 }
 
 export default NavBar;
-
