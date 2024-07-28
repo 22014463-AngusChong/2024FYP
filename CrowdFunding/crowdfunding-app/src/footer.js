80cd49d20e374c99f3a54bbf304ddb5e33b50a04
GitHub_username <akileshnelson@gmail.com>
2024-07-29 04:34:07 +0800

    update

--- CrowdFunding/crowdfunding-app/src/footer.js
+++ CrowdFunding/crowdfunding-app/src/footer.js
@@ -3,21 +3,7 @@ function Footer() {
         <div>
             <footer className="footer mt-5 mb-5">
                 <div className="text-center">
-                    <span className="text-muted">© 2024 Your GetFund. All rights reserved.</span>
-                </div>
-                <div className="social-media-icons text-center">
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-linkedin-in"></i>
-                    </a>
+                    <span className="text-muted">© 2024 GetFund. All rights reserved.</span>
                 </div>
             </footer>      
         </div>
